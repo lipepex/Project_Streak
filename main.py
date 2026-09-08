02fd75c53bb6a3b76f3746ea30ff8de83819f3f5
Ricardo Lourenço <161585835+lipepex@users.noreply.github.com>
2026-09-08 02:01:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#Modulos
+
+#Class
+
+    #Construtor
+    
+    #Objeto e Design
+
+    #App Settings
+
+    #Button Events
+
+    #Translate Click
+
+    #Reset App
+
+    #Translate text (Google)
+
+    #Reverse Translation
+
+#Main Run
